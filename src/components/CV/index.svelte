c8792e4857a3de3eff9263d46cd86f9eb88adaf7
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-03-25 00:56:28 +0100

    WIP: CV
    1. nav
    2. foreword
    3. stack
    4. added build status badge
    
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/components/CV/index.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Foreword from './Foreword.svelte';
+  import Stack from './Stack.svelte';
+
+  export let data = null;
+</script>
+
+<style>
+  #cv {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-auto-rows: 1fr 2fr;
+    grid-template-areas:
+      'foreword'
+      'stack'
+    ;
+  }
+</style>
+
+<article id='cv'>
+  {#if data}
+    {#if data.foreword}
+      <Foreword gridArea='foreword'>{data.foreword}</Foreword>
+    {/if}
+    {#if data.stack}
+      <Stack gridArea='stack' data={data.stack} />
+    {/if}
+  {/if}
+</article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,19 @@
+<script>
+	import CV from '../components/CV/index.svelte';
+	import { onMount } from 'svelte';
+
+	let data = null;
+
+	onMount(async () => {
+		data = await (await fetch('/data/cv.json')).json();
+	});
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Dmitry N. Medvedev</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<CV {data} />
