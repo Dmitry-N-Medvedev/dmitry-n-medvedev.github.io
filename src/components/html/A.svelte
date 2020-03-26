480e949e1417676138072653bfb117b0ece19c26
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-03-26 19:44:29 +0100

    fixed: contacts
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/components/html/A.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let href = null;
+  export let target = '_blank';
+
+  const handleClick = (evt) => {
+    const { protocol, pathname } = evt.srcElement;
+
+    console.debug('A on:click', protocol, pathname);
+  };
+</script>
+
+<a {href} {target} on:click={handleClick}>
+  <slot>N/A</slot>
+</a>
---
+++ src/components/html/Icon.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let src = '';
+  export let alt = '';
+</script>
+
+<style>
+  .icon {
+    width: 2rem;
+    height: 2rem;
+  }
+</style>
+
+<img {src} {alt} class='icon' />
