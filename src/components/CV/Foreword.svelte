5685c6fe2e5f359111676900f301cb46a81d3a6b
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-03-27 14:43:21 +0100

    fix foreword grid placement
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/components/CV/Foreword.svelte
+++ src/components/CV/Foreword.svelte
@@ -1,22 +1,5 @@
-<script>
-  import { onMount } from 'svelte';
-
-  export let gridArea = null;
-  let root = null;
-
-  onMount(() => {
-    root = document.documentElement;
-
-    root.style.setProperty('--foreword-grid-area', gridArea);
-  });
-</script>
-
 <style>
-  :root {
-    --foreword-grid-area: '';
-  }
   #foreword {
-    grid-area: var(--foreword-grid-area);
     display: flex;
     padding: 1vh 1.5vw;
   }
