5f57297ef45995019c26653032bc3f14be86fd97
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-03-27 13:55:16 +0100

    added my photo to contacts section
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/components/CV/Contacts.svelte
+++ src/components/CV/Contacts.svelte
@@ -9,10 +9,34 @@
 <style>
   .contacts {
     display: grid;
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 1fr;
     grid-template-rows: min-content min-content min-content 6vh min-content;
+    grid-template-areas:
+      'contacts-photo contacts-content'
+      'contacts-photo contacts-content'
+      'contacts-photo contacts-content'
+      'contacts-photo contacts-content'
+      'contacts-photo contacts-content'
+    ;
     grid-gap: calc(var(--layout-padding) * 2) 0;
     font-size: calc(var(--font-size) * 1.25);
+  }
+
+  .contacts-photo {
+    grid-area: contacts-photo;
+    display: flex;
+    justify-content: flex-start;
+    padding: 0 0 0 1.5vw;
+  }
+
+  .contacts-photo > img {
+    width: 15vw;
+    height: 15vw;
+    filter: drop-shadow(0px 0px 3px var(--font-color));
+  }
+
+  .contacts-content {
+    grid-area: contacts-content;
   }
 
   :global(.social-network-icons) {
@@ -30,18 +54,23 @@
 </style>
 
 <article class='contacts'>
-  <GridBlockRecord className='display-flex justify-content-flex-end'><span itemprop='name'>{data.name}</span></GridBlockRecord>
-  <GridBlockRecord className='display-flex justify-content-flex-end'>{data.city}</GridBlockRecord>
-  <GridBlockRecord className='display-flex justify-content-flex-end'>{data.country}</GridBlockRecord>
-  <GridBlockRecord className='display-flex justify-content-flex-end'>
-    <A href='mailto:{data.email}'><span itemprop='email'>{data.email}</span></A>
-  </GridBlockRecord>
-  <GridBlockRecord className='display-flex justify-content-flex-end social-network-icons'>
-    <!-- see: https://www.flaticon.com/packs/social-circles -->
-    {#each data.links as { name, url, svg }}
-      <a class='a' href={url} target='_blank'>
-        <Icon src='{svg}' alt='{name}' />
-      </a>
-    {/each}
-  </GridBlockRecord>
+  <div class='contacts-photo'>
+    <img src='/photos/me.jpg' alt='me' />
+  </div>
+  <div class='contacts-content'>
+    <GridBlockRecord className='display-flex justify-content-flex-end'><span itemprop='name'>{data.name}</span></GridBlockRecord>
+    <GridBlockRecord className='display-flex justify-content-flex-end'>{data.city}</GridBlockRecord>
+    <GridBlockRecord className='display-flex justify-content-flex-end'>{data.country}</GridBlockRecord>
+    <GridBlockRecord className='display-flex justify-content-flex-end'>
+      <A href='mailto:{data.email}'><span itemprop='email'>{data.email}</span></A>
+    </GridBlockRecord>
+    <GridBlockRecord className='display-flex justify-content-flex-end social-network-icons'>
+      <!-- see: https://www.flaticon.com/packs/social-circles -->
+      {#each data.links as { name, url, svg }}
+        <a class='a' href={url} target='_blank'>
+          <Icon src='{svg}' alt='{name}' />
+        </a>
+      {/each}
+    </GridBlockRecord>
+  </div>
 </article>
